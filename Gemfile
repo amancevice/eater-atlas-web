eeb91d61bde68b58d9038ba1ae242b762a806b9d
Alexander Mancevice <amancevice@cargometrics.com>
2016-04-03 14:25:40 -0400

    init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+ruby '2.3.0'
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-asset-pipeline', require: ['sinatra/asset_pipeline']
+gem 'sinatra-contrib',        require: ['sinatra/cookies']
---
+++ app.rb
@@ -0,0 +1,46 @@
+require 'bundler/setup'
+Bundler.require
+
+module EaterAtlas
+  class Web < Sinatra::Base
+    configure do
+      enable :sessions
+      set :assets_precompile, %w(application.js styles.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+      set :assets_css_compressor, :sass
+      helpers  Sinatra::Cookies
+      register Sinatra::AssetPipeline
+
+      # Actual Rails Assets integration, everything else is Sprockets
+      if defined?(RailsAssets)
+        RailsAssets.load_paths.each do |path|
+          settings.sprockets.append_path(path)
+        end
+      end
+    end
+
+    def self.title
+      "EaterAtlas"
+    end
+
+
+    get '/' do
+      weekday = Time.now.strftime("%A")
+      meal    = "Lunch"
+
+      redirect "/#{weekday}/#{meal}"
+    end
+
+    get '/:weekday' do |weekday|
+      meal = "Lunch"
+
+      redirect "#{weekday}/#{meal}"
+    end
+
+    get '/:weekday/:meal' do |weekday, meal|
+      @weekday = weekday
+      @meal    = meal
+
+      erb :index
+    end
+  end
+end
